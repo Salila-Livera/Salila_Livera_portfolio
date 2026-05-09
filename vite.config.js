d4ece984eb1e9e7799ee86cb9e4c4025d0b7fb90
Salila-Livera <salliladimuthu2002@gmail.com>
2026-05-10 00:05:04 +0530

    chore: update base path and add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Livera_Portfolio/',
+  base: '/Salila_Livera_portfolio/',
   plugins: [
     react(),
     tailwindcss(),
